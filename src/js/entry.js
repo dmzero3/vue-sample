10970fffa4b4a12515d408ef36b8a4839cccbef5
松浦 大 <matsuura_dai@cyberagent.co.jp>
2015-11-02 19:46:12 +0900

    Added alert function for test

--- src/js/entry.js
+++ src/js/entry.js
@@ -3,6 +3,18 @@
   'use strict';
   var Vue = require('vue')
   console.log(Vue);
+
+  var registerVM = new Vue({
+    el: '.m-register',
+    data: {},
+    methods: {
+      alert: function(){
+        alert('alert');
+      }
+    }
+  });
+
+
   require('./components/todo-register')
 
 
