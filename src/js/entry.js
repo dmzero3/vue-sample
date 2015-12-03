17150c57a0e557383aa2f3446a77abb9f9b187d5
松浦 大 <matsuura_dai@cyberagent.co.jp>
2015-12-03 19:14:53 +0900

    Added event list of atnd

--- src/js/entry.js
+++ src/js/entry.js
@@ -2,20 +2,46 @@
 
   'use strict';
   var Vue = require('vue')
-  console.log(Vue);
 
-  var registerVM = new Vue({
-    el: '.m-register',
-    data: {},
+  var atndVM = new Vue({
+    el: '#vm-atnd',
+    data: {
+      items: [
+        {
+          event: {
+            title: 'ダミータイトル1'
+          }
+        },
+        {
+          event: {
+            title: 'ダミータイトル2'
+          }
+        }
+      ]
+    },
     methods: {
-      alert: function(){
-        alert('alert');
+      renderAtnd: function(){
+        var self = this;
+        var API_URL = 'http://api.atnd.org/events/?format=jsonp';
+        $.ajax({
+          type: 'GET',
+          url: API_URL,
+          dataType: 'jsonp',
+          success: function(data){
+
+            var items = data.events;
+            self.$data.items = items;
+          },
+          error: function(){
+
+          }
+        });
       }
     }
   });
 
 
-  require('./components/todo-register')
+  // require('./components/todo-register')
 
 
 })();
